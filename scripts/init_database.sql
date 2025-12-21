3ffa6ed07935fb0f397380acf60ba4412c09db27
Gaurav Menaria <164004292+Gaurav-111@users.noreply.github.com>
2025-12-21 15:06:49 +0530

    Create init_database.sql for DataWarehouse setup
    This script creates the DataWarehouse database and sets up three schemas: bronze, silver, and gold.

---
+++ scripts/init_database.sql
@@ -0,0 +1,12 @@
+/*
+**purpose of this script** - It's our first script by which we create our database named **"DataWarehouse**" and we this script setup three schema whin the database: 'bronze','silver and 'gold' 
+
+*/
+
+create database DataWarehouse
+
+create schema bronze 
+
+create schema silver
+
+create schema gold
